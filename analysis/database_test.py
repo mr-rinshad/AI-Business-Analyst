86376f330b37144f7505542ecbe6b01b2038b3de
mr-rinshad <mrmuhammedrinshad@gmail.com>
2026-08-22 11:01:23 +0530

    project setup

---
+++ analysis/database_test.py
@@ -0,0 +1,16 @@
+import os
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+connection = mysql.connector.connect(
+    host=os.getenv("MYSQL_HOST"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    database=os.getenv("MYSQL_DATABASE")
+)
+
+print("Database connected successfully!")
+
+connection.close()
